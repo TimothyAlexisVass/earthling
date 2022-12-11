2043fd689bbfe950a3c54aa14e91ddff55ef245c
Timothy Alexis Vass <timvas@bredband2.se>
2022-12-11 17:34:59 +0100

    Add food_list to show view for nutrition tracker

--- app/controllers/nutrition_controller.rb
+++ app/controllers/nutrition_controller.rb
@@ -1,4 +1,15 @@
 class NutritionController < ApplicationController
   def show
+    @user = User.first
+    @food_list = get_food_list
+  end
+
+  def get_food_list
+    SelectedFoodItem.where(user_id: @user.id).map{ |e|
+      {
+        data: FoodItem.where(id: e.food_item_id).first,
+        amount: e.amount
+      }
+    }
   end
 end
