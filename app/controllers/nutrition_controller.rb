5fb9f769bffbdf5d12446c7d36e375cbbb3a1f9b
Timothy Alexis Vass <timvas@bredband2.se>
2022-12-11 16:53:18 +0100

    Begin nutrition tracking module

---
+++ app/controllers/nutrition_controller.rb
@@ -0,0 +1,4 @@
+class NutritionController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/coaching', to: 'users#new'
 
+  get '/nutrition', to: 'nutrition#show'
+
   resources :users, only: [:new, :create]
   resources :food_items, only: [:new, :create]
   resources :selected_food_items
