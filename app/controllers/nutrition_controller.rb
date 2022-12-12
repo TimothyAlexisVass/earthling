9ecd504ad4f162e60dfa427c0e986cd43c1e5edd
Timothy Alexis Vass <timvas@bredband2.se>
2022-12-12 09:42:01 +0100

    Add energy calculation to food list. Also add total_energy and total_amount methods to NutritionController

--- app/controllers/nutrition_controller.rb
+++ app/controllers/nutrition_controller.rb
@@ -2,14 +2,27 @@ class NutritionController < ApplicationController
   def show
     @user = User.first
     @food_list = get_food_list
+    @total_amount = total_amount
+    @total_energy = total_energy
   end
 
-  def get_food_list
-    SelectedFoodItem.where(user_id: @user.id).map{ |e|
-      {
-        data: FoodItem.where(id: e.food_item_id).first,
-        amount: e.amount
+  private
+
+    def get_food_list
+      list = SelectedFoodItem.where(user_id: @user.id).map{ |selected_food_item|
+        {
+          data: (food_item=FoodItem.where(id: selected_food_item.food_item_id).first),
+          amount: selected_food_item.amount,
+          energy: food_item[:energy]*selected_food_item.amount/100.0
+        }
       }
-    }
-  end
+    end
+
+    def total_amount
+      @food_list.sum{ |item| item[:amount] }
+    end
+
+    def total_energy
+      @food_list.sum{ |item| item[:data][:energy].to_f }
+    end
 end
