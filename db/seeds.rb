e00d9e29ca0110fcf799a55fdd30d8a7f7b9b63a
TimothyAlexisVass <tim@earthling.se>
2023-07-14 17:11:04 +0000

    Add sql data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Load the food_items SQL file
+sql_file = File.read('db/data/food_items.sql')
+
+# Execute the SQL file
+ActiveRecord::Base.connection.execute(sql_file)
